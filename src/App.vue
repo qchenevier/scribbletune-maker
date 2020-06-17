da5978e6ca13a12ff89ade08701181747437a39f
Quentin Chenevier <qchenevier@users.noreply.github.com>
2020-06-17 23:38:21 +0200

    refactor Instrument component

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,20 @@
   <div id="app">
     <h1>Scribbletune playground</h1>
     <PlayPauseButton @playPause="tonePlayPause" />
-    <InstrumentChoice @choice="createInstrument" />
-    <button @click="updateParams">
-      Update params
-    </button>
-    <AceEditor
-      :showPrintMargin="true"
-      :showGutter="true"
-      :highlightActiveLine="true"
-      :value="JSON.stringify(instrumentParams, null, 2)"
-      :enableBasicAutocompletion="true"
-      width="350"
-      mode="json"
-      theme="dawn"
-      :onChange="storeParams"
-      name="editor"
-      :editorProps="{ $blockScrolling: true }"
-    />
+    <Instrument @instrument="createInstrument" />
   </div>
 </template>
 
 <script>
 import PlayPauseButton from "./PlayPauseButton.vue";
-import InstrumentChoice from "./InstrumentChoice.vue";
+import Instrument from "./Instrument.vue";
 import createSession from "./session.js";
 
-import brace from "brace";
-import { Ace as AceEditor } from "vue2-brace-editor";
-import "brace/mode/json";
-import "brace/theme/dawn";
-
 export default {
-  components: { PlayPauseButton, InstrumentChoice, AceEditor },
+  components: { PlayPauseButton, Instrument },
   data() {
     return {
       instrument: undefined,
-      instrumentParams: undefined,
-      instrumentParamsInput: undefined,
       session: undefined
     };
   },
@@ -50,17 +27,8 @@ export default {
         Tone.Transport.stop();
       }
     },
-    createInstrument: function(instrumentName) {
-      let instrumentTemplate = new Tone[instrumentName]();
-      this.instrumentParams = instrumentTemplate.get();
-      this.instrument = new Tone[instrumentName](this.instrumentParams);
-    },
-    storeParams: function(jsonString) {
-      this.instrumentParamsInput = jsonString;
-    },
-    updateParams: function() {
-      this.instrumentParams = JSON.parse(this.instrumentParamsInput);
-      this.instrument.set(this.instrumentParams);
+    createInstrument: function(instrument) {
+      this.instrument = instrument;
     }
   },
   watch: {
---
+++ src/Instrument.vue
@@ -0,0 +1,65 @@
+<template lang="html">
+  <div>
+    <InstrumentChoice @choice="createInstrument" />
+    <button @click="updateParams">
+      Update params
+    </button>
+    <AceEditor
+      :showPrintMargin="true"
+      :showGutter="true"
+      :highlightActiveLine="true"
+      :value="JSON.stringify(instrumentParams, null, 2)"
+      :enableBasicAutocompletion="true"
+      width="350"
+      mode="json"
+      theme="dawn"
+      :onChange="storeParams"
+      name="editor"
+      :editorProps="{ $blockScrolling: true }"
+    />
+  </div>
+</template>
+
+<script>
+import InstrumentChoice from "./InstrumentChoice.vue";
+
+import brace from "brace";
+import { Ace as AceEditor } from "vue2-brace-editor";
+import "brace/mode/json";
+import "brace/theme/dawn";
+
+export default {
+  components: { InstrumentChoice, AceEditor },
+  data() {
+    return {
+      instrument: undefined,
+      instrumentParams: undefined,
+      instrumentParamsInput: undefined
+    };
+  },
+  methods: {
+    createInstrument: function(instrumentName) {
+      let instrumentTemplate = new Tone[instrumentName]();
+      this.instrumentParams = instrumentTemplate.get();
+      this.instrument = new Tone[instrumentName](this.instrumentParams);
+    },
+    storeParams: function(jsonString) {
+      this.instrumentParamsInput = jsonString;
+    },
+    updateParams: function() {
+      this.instrumentParams = JSON.parse(this.instrumentParamsInput);
+      this.instrument.set(this.instrumentParams);
+    }
+  },
+  watch: {
+    instrument: {
+      handler: function() {
+        this.$emit("instrument", this.instrument);
+      },
+      deep: true
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped></style>
