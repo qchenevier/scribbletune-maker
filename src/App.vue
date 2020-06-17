8ad09c0fc1fec4ab2013b4ddc0ae275ff845b4f9
Quentin Chenevier <qchenevier@users.noreply.github.com>
2020-06-17 09:47:17 +0200

    first try with PlayPauseButtons component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,45 @@
   <div id="app">
     <input v-model="message" type="text" />
     <h2 class="message">{{ message }}</h2>
-    <h3>Un autre message...</h3>
+    <h3>Un autre message</h3>
+    <PlayPauseButton @playPause="playPause" />
   </div>
 </template>
 
 <script>
 import * as scribble from "scribbletune";
+import PlayPauseButton from "./PlayPauseButton.vue";
+
+const session = new scribble.Session();
+const instrument = new Tone.Synth();
+const channel = session.createChannel({
+  idx: 0,
+  instrument: instrument,
+  clips: [
+    {
+      notes: "CM-3 ".repeat(4) + "DM7-3 ".repeat(4) + "GM-2 ".repeat(4),
+      pattern: "[xx_-]".repeat(12)
+    }
+  ]
+});
+
+session.startRow(0);
 
 export default {
+  components: { PlayPauseButton },
   data() {
     return {
       message: "Hello World"
     };
+  },
+  methods: {
+    playPause: function(play) {
+      if (play) {
+        Tone.Transport.start();
+      } else {
+        Tone.Transport.stop();
+      }
+    }
   }
 };
 </script>
---
+++ src/PlayPauseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button :class="buttonClass" @click="toggle">
+    <slot>{{ buttonMsg }}</slot>
+  </button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isPlaying: false
+    };
+  },
+  computed: {
+    buttonMsg: function() {
+      return this.isPlaying ? "Play" : "Pause";
+    },
+    buttonClass: function() {
+      return this.isPlaying ? "play" : "pause";
+    }
+  },
+  methods: {
+    toggle: function() {
+      this.isPlaying = !this.isPlaying;
+      this.$emit("playPause", this.isPlaying);
+    }
+  }
+};
+</script>
+
+<style>
+.play {
+  color: green;
+}
+
+.pause {
+  color: grey;
+}
+</style>
