567037d31f568b6f2fa50c3a67f3858a35276504
Quentin Chenevier <qchenevier@users.noreply.github.com>
2020-08-02 22:23:41 +0200

    [fix] deployment base route

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import "@mdi/font/css/materialdesignicons.css";
 import Buefy from "buefy";
 import VueClipboard from "vue-clipboard2";
 
-// VueClipboard.config.autoSetContainer = true // add this line
 Vue.use(VueClipboard);
 Vue.use(VueRouter);
 Vue.use(Buefy);
 
 const router = new VueRouter({
-  // mode: "history",
+  mode: "history",
   routes: [{ path: "/", name: "App", component: App }],
   base: "/scribbletune-maker/"
 });
