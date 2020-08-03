cc016b9107902d3cc85208e980b3872d90ee849f
Quentin Chenevier <qchenevier@users.noreply.github.com>
2020-08-03 12:51:17 +0200

    Update config with CNAME for scribbleton.live

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,14 @@
 const merge = require("webpack-merge");
 const mainConfig = require("./webpack.config.js");
+const CnameWebpackPlugin = require("cname-webpack-plugin");
 
 module.exports = merge(mainConfig, {
   mode: "production",
+  plugins: [
+    new CnameWebpackPlugin({
+      domain: "scribbleton.live"
+    })
+  ],
   optimization: {
     splitChunks: {
       cacheGroups: {
